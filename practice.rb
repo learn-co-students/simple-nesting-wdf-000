54a59968bf110111fc1944912edb8acbcb97fb8f
Hanna Redleaf <hredleaf@gmail.com>
2016-06-27 18:32:05 +0000

    Done.

---
+++ practice.rb
@@ -0,0 +1,49 @@
+def changing_alan
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     #change what Alan Kay is :known_for the value of the alans_new_info variable.
+     alans_new_info = "GUI"
+
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+
+		 return programmer_hash[:alan_kay]
+
+end
+
+print changing_alan
+
+def adding_to_dennis
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 return programmer_hash[:dennis_ritchie]
+end
+
+print adding_to_dennis
